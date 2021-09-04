1a17b35123ee2c91201f6e6f756c9fc2cec22955
Wricheek <wricheekadhikari1@gmail.com>
2021-09-04 14:35:37 +0530

    material_floating search bar loaded

--- national_anthems/lib/pages/dashboard.dart
+++ national_anthems/lib/pages/dashboard.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:floating_search_bar/floating_search_bar.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -14,26 +13,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: FloatingSearchBar.builder(
-        itemCount: 100,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: Text(index.toString()),
-          );
-        },
-        trailing: CircleAvatar(
-          child: Text("RD"),
-        ),
-        drawer: Drawer(
-          child: Container(),
-        ),
-        onChanged: (String value) {},
-        onTap: () {},
-        decoration: InputDecoration.collapsed(
-          hintText: "Search...",
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
